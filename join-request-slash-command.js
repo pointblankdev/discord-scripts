91e5b5aec2a8a7a393c480e1bff8913475266b01
Mouzayan <42501101+Mouzayan@users.noreply.github.com>
2021-04-13 17:14:57 -0400

    join-request-slash-command.js

---
+++ join-request-slash-command.js
@@ -0,0 +1,43 @@
+/// https://www.npmjs.com/package/discord-slash-commands-client
+const interactions = require('discord-slash-commands-client');
+require('dotenv').config();
+
+const client = new interactions.Client(
+  process.env.TOKEN,
+  process.env.CLIENT_ID
+);
+
+// list all your existing commands.
+client.getCommands().then(console.log).catch(console.error);
+
+const command = {
+  name: 'join request',
+  description: 'Send a join request',
+  options: [
+    {
+      name: 'list',
+      description: 'The list of teams to join',
+      type: 1,
+      required: true,
+      choices: [
+        {
+          name: 'Heats',
+          value: 'team_heats',
+        },
+        {
+          name: 'Free for All',
+          value: 'team_freeforall',
+        },
+      ],
+    },
+    {
+      name: 'limit',
+      description: 'Number of teams shown',
+      type: 5,
+      required: false,
+    },
+  ],
+};
+
+// will create a new command and log its data. If a command with this name already exist will that be overwritten.
+client.createCommand(command).then(console.log).catch(console.error);
