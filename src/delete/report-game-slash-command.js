19f32d1bc8e888eccbb2e6b0bf05b7682cba7c6b
Mouzayan <42501101+Mouzayan@users.noreply.github.com>
2021-04-14 11:24:58 -0400

    created report-game command

---
+++ src/delete/report-game-slash-command.js
@@ -0,0 +1,50 @@
+/// https://www.npmjs.com/package/discord-slash-commands-client
+const interactions = require('discord-slash-commands-client');
+require('dotenv').config();
+
+const client = new interactions.Client(
+  process.env.TOKEN,
+  process.env.CLIENT_ID
+);
+
+// list all your existing commands.
+client.getCommands().then(console.log).catch(console.error);
+
+const command = {
+  name: 'report-game',
+  description: 'Report game scores',
+  options: [
+    {
+      name: 'list',
+      description: 'The list of games',
+      type: 3,
+      required: true,
+      choices: [
+        {
+          name: 'Fortnite',
+          value: 'game_fortnite',
+        },
+        {
+          name: 'Apex Legends',
+          value: 'game_apexlegends',
+        },
+        {
+          name: 'Counter Strike',
+          value: 'game_counterstrike',
+        },
+      ],
+    },
+    {
+      name: 'limit',
+      description: 'Number of games shown',
+      type: 5,
+      required: false,
+    },
+  ],
+};
+
+//will delete command
+client
+  .deleteCommand('831911609240125520')
+  .then(console.log)
+  .catch(console.error);
