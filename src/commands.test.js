7954683809501ac63e28a8930f0735ffc12866b1
Al-Kassim Kassim <alkassimk@gmail.com>
2021-06-01 16:31:56 +0100

    Revert ":sparkles: Ephemeral messages"
    This reverts commit 880cc1fec8c323441db0858bdc985070e6062ef6.

--- src/commands.test.js
+++ src/commands.test.js
@@ -1,9 +1,10 @@
-const client = require('./Commands');
+const client = require("./Commands");
 
-test('should get commands', async () => {
+
+test("should get commands", async () => {
   console.log(await client.getCommands({}));
 });
 
-test('should delete command by id', async () => {
-  console.log(await client.deleteCommand('849198587418378250'));
-});
+// test("should delete command by id", async () => {
+//   console.log(await client.deleteCommand('849198971103608872'));
+// });
