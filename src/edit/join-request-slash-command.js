0fd228bc82f310696882bdf0d1e9600ce7f8e147
Al-Kassim Kassim <alkassimk@gmail.com>
2021-06-01 19:15:36 +0100

    fix participants query

--- src/edit/join-request-slash-command.js
+++ src/edit/join-request-slash-command.js
@@ -11,7 +11,7 @@ const client = new interactions.Client(
 client.getCommands().then(console.log).catch(console.error);
 
 const command = {
-  name: 'join-requests',
+  name: 'join',
   description: 'Select a join request',
   options: [
     {
