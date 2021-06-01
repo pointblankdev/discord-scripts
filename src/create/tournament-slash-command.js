8acfea015508daf79eaceeddaae2bbccd12fb38f
Al-Kassim Kassim <alkassimk@gmail.com>
2021-06-01 15:43:06 +0100

    :building_construction: Test Embed Folder

--- src/create/tournament-slash-command.js
+++ src/create/tournament-slash-command.js
@@ -12,7 +12,7 @@ client.getCommands().then(console.log).catch(console.error);
 
 const command = {
   name: 'tournament',
-  description: 'Select a tournament',
+  description: 'Select a tournament (edited)',
   options: [
     {
       name: 'list',
